c5f4073d43342df304c12d8eb19f15e2fef0ffbf
ZhaohuiZh <bravezhaohui@gmail.com>
2013-09-24 13:07:16 -0400

    add client

---
+++ tsp/client.py
@@ -0,0 +1,40 @@
+'''
+Created on Sep 20, 2013
+
+@author: shaqal
+@modified by zhaohui
+'''
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('127.0.0.1', 5006))
+ques1=s.recv(1024)
+
+if ques1=="Team_Name?":
+    s.send("OFF BY ONE")
+
+input_data=''
+
+while True:
+    chunk = s.recv(1000000)
+    if not chunk: break
+    if chunk == '':
+        raise RuntimeError("socket connection broken")
+    input_data = input_data + chunk
+    if ';' in input_data:
+        break
+
+#calculate your solution is less than 2 minutes
+solution = "1 2;"
+
+totalsent=0
+MSGLEN = len(solution) 
+while totalsent < MSGLEN:
+
+    sent = s.send(solution[totalsent:])
+    if sent == 0:
+        raise RuntimeError("socket connection broken")
+    totalsent = totalsent + sent
+
+s.close()
