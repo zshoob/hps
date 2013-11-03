b43434dfa93a8b1784449b05bfc918cd8d5a0bd2
githulhu <schubert.zach@gmail.com>
2013-11-03 13:11:14 -0500

    Update Muncher.java

--- nano/Muncher.java
+++ nano/Muncher.java
@@ -1,61 +1,39 @@
 import java.util.LinkedList;
 
-public class Muncher {
-	public Muncher( Node loc, String program, int counter ) {
-		this.loc = loc;
-		this.counter = counter;
-		this.program = program;
-		this.starved = false;
+public class Node {
+	public Node( int id, int xloc, int yloc ) {
+		this.id = id;
+		this.xloc = xloc;
+		this.yloc = yloc;	
+		this.munched = false;
 	}
-	public Muncher( Muncher o ) {
-		this.loc = new Node(o.loc);
-		this.counter = o.counter;
-		this.program = o.program;
-		this.starved = o.starved;
+	public Node( Node o ) {
+		this.id = o.id;
+		this.xloc = o.xloc;
+		this.yloc = o.yloc;
+		this.munched = o.munched;
 	}
-	public void run( ) {
-		this.loc.munched = true;	
-		boolean starve = true;
-		for( int i = 0; i < 4; i++ ) {
-			char instruction = this.program.charAt(this.counter);
-			Node node = null;
-			switch( instruction ) {
-				case 'u': node = this.loc.up; break;
-				case 'd': node = this.loc.down; break;				
-				case 'l': node = this.loc.left; break;				
-				case 'r': node = this.loc.right; break;				
-			}
-			this.counter = (this.counter + 1) % 4;
-			if( !(node == null || node.munched) ) {
-				System.out.println(instruction);
-				starve = false;
-				this.loc = node;
-				break;
-			}
-		}
-		if( starve ) {
-			this.starved = true;
-		}
+	LinkedList<Node> activeSiblings( ) {
+		LinkedList<Node> sibs = new LinkedList<Node>( );
+		if( this.up != null && !this.up.munched )
+			sibs.add(this.up);
+		if( this.down != null && !this.down.munched )
+			sibs.add(this.down);
+		if( this.left != null && !this.left.munched )
+			sibs.add(this.left);
+		if( this.right != null && !this.right.munched )
+			sibs.add(this.right);			
+		return sibs;						
 	}
-	static String[ ] allPrograms( ) {
-		char dirs[ ] = new char[ ]{'u','d','l','r'};
-		LinkedList<String> i = new LinkedList<String>( );
-		for( char c : dirs )
-			i.add("" + c);
-		LinkedList<String> j;
-		for( int loop = 0; loop < 3; loop++ ) {
-			j = new LinkedList<String>( );
-			for( String str : i ) 
-				for( char c : dirs )
-					if( str.indexOf( c ) < 0 )
-						j.add( str + c );
-			i = j;
-		}
-		return i.toArray(new String[0]);
-	}	
-	Node loc;
-	int counter;
-	final String program;
-	boolean starved;
-	static String[ ] allPrograms = allPrograms( );	
+	public void view( ) {
+		System.out.println( this.id + ":\t(" + this.xloc + "," + this.yloc + ")" );
+	}
+	int id;
+	int xloc;
+	int yloc;
+	boolean munched;	
+	Node up;
+	Node down;
+	Node left;
+	Node right;
 }
