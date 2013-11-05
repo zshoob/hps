fd054b7c35cc754053ca4753605fe30f91c67fc2
githulhu <schubert.zach@gmail.com>
2013-11-05 12:58:19 -0500

    Update OffByOne.java

--- nano/OffByOne.java
+++ nano/OffByOne.java
@@ -22,7 +22,6 @@ public class OffByOne {
         out = new PrintWriter(client.getOutputStream( ), true);
         in = new BufferedReader(new InputStreamReader(client.getInputStream( )));        
         send("OffByOne");
-        //send("0");
         State state = new State(receive( ));
         while(true) {
         	String input = receive( );
